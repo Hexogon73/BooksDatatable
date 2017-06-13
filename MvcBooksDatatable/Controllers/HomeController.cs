cc0b869e36dc8c24cdbe936299f661a7ebc2c9ca
Hexogon73 <sergeylukianoff@yandex.ru>
2017-06-13 14:13:40 +0400

    step 12

--- MvcBooksDatatable/Controllers/HomeController.cs
+++ MvcBooksDatatable/Controllers/HomeController.cs
@@ -14,8 +14,35 @@ namespace MvcBooksDatatable.Controllers
         }
         public ActionResult getData()
         {
+            //Datatable parameter
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            //pading parameter
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            //sorting parameter
+            var sortColumn = Request.Form.GetValues("columns["+Request.Form.GetValues("order[0][column]").FirstOrDefault()+"][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            //filter parameter
+            var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+
+
+            List<Book> allBooks = new List<Book>();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            //database query
             using (BooksDatabaseEntities dc = new BooksDatabaseEntities())
             {
+                var v = (from a in dc.Books select a);
+
+                //search
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+
+                }
+
                 return Json(dc.Books.ToList());
             }
         }
--- MvcBooksDatatable/Scripts/myApp.js
+++ MvcBooksDatatable/Scripts/myApp.js
@@ -1,21 +1,26 @@
-﻿var app = angular.module('MyApp',['datatables']);
-app.controller('homeCtrl',['$scope', '$http', 'DTOpionsBuilder', 'DTColumnBuilder',
-    function ($scope, $http, DTOpionsBuilder, DTColumnBuilder) {
+﻿var app = angular.module('MyApp', ['datatables']);
+app.controller('homeCtrl', ['$scope', '$http', 'DTOptionsBuilder', 'DTColumnBuilder',
+    function ($scope, $http, DTOptionsBuilder, DTColumnBuilder) {
         $scope.dtColumns = [
-            DTColumnBuilder.newColumn("BookID", "Book ID"),
-            DTColumnBuilder.newColumn("Name", "Name"),
-            DTColumnBuilder.newColumn("Author", "Author"),
-            DTColumnBuilder.newColumn("Year", "Year"),
-            DTColumnBuilder.newColumn("Genre", "Genre"),
-            DTColumnBuilder.newColumn("Page", "Page"),
-            DTColumnBuilder.newColumn("Price", "Price"),
-            DTColumnBuilder.newColumn("Country", "Country")
+            //here We will add .withOption('name','column_name') for send column name to the server 
+            DTColumnBuilder.newColumn("BookID", "Book ID").withOption('name', 'BookID'),
+            DTColumnBuilder.newColumn("Name", "Name").withOption('name', 'Name'),
+            DTColumnBuilder.newColumn("Author", "Author").withOption('name', 'Author'),
+            DTColumnBuilder.newColumn("Year", "Year").withOption('name', 'Year'),
+            DTColumnBuilder.newColumn("Genre", "Genre").withOption('name', 'Genre'),
+            DTColumnBuilder.newColumn("Page", "Page").withOption('name', 'Page'),
+            DTColumnBuilder.newColumn("Price", "Price").withOption('name', 'Price'),
+            DTColumnBuilder.newColumn("Country", "Country").withOption('name', 'Country')
         ]
 
         $scope.dtOpions = DTOptionsBuilder.newOptions().withOption('ajax', {
+            dataSrc: "data",
             url: "/home/getdata",
             type: "POST"
         })
-        .withPaginationType('full_numbers')
-        .withDisplayLength(10);
+        .withOption('processing', true) //for show progress bar
+        .withOption('serverSide', true) // for server side processing
+        .withPaginationType('full_numbers') // for get full pagination options // first / last / prev / next and page numbers
+        .withDisplayLength(10) // Page size
+        .withOption('aaSorting', [0, 'asc']) // for default sorting column // here 0 means first column
     }])
